76da2b5dd136b2b970bc39efd2db29de4da64df8
Jason Dyck <j.dyck@redskytech.io>
2025-10-20 21:00:46 -0600

    Added TCP port check for each host

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -14,12 +17,23 @@ func main() {
 	intervalPtr := flag.Int("interval", 5000, "The interval on which to check each host in milliseconds. Defaults to 5000.")
 	flag.Parse()
 
-	fmt.Println(*hostListPtr)
 	trimmedList := strings.TrimSpace(*hostListPtr)
 	if trimmedList == "" {
 		panic("No hosts provided")
 	}
-	fmt.Println("Preparing to monitor port ", *portPtr, " every ", *intervalPtr, " milliseconds on hosts", *hostListPtr)
+	if *portPtr <= 0 || *portPtr > 65535 {
+		panic("Invalid port provided")
+	}
+	if *intervalPtr <= 0 {
+		panic("Invalid interval provided")
+	}
+	interval := time.Duration(*intervalPtr * 1000 * 1000)
+
+	fmt.Println("Preparing to monitor port", *portPtr, "every", *intervalPtr, "milliseconds on hosts", *hostListPtr)
+
+	for host := range strings.SplitSeq(*hostListPtr, " ") {
+		go checkHost(host, *portPtr, interval)
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, world!")
@@ -27,4 +41,19 @@ func main() {
 
 	fmt.Println("Server is running at localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func checkHost(hostname string, port int, interval time.Duration) {
+	for {
+		portStr := strconv.Itoa(port)
+		conn, err := net.DialTimeout("tcp", net.JoinHostPort(hostname, portStr), time.Second)
+		if err != nil {
+			fmt.Println("Connection error:", hostname, err)
+		}
+		if conn != nil {
+			fmt.Println("Opened", net.JoinHostPort(hostname, portStr))
+			conn.Close()
+		}
+		time.Sleep(interval)
+	}
 }
