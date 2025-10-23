d40ec8e95dc4e8a77c2c75876a29eaf4b3fd8fa4
Jason Dyck <j.dyck@redskytech.io>
2025-10-22 22:44:49 -0600

    Added logs for check calls Added calculating host stats from logs Currently blocking somewhere after first host check

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ const (
 	HostStatusOffline  HostStatus = "Offline"
 )
 
-type host struct {
+type Host struct {
 	Hostname                 string
 	Status                   HostStatus
 	LastSeen                 time.Time
@@ -29,10 +29,20 @@ type host struct {
 	FiveMinuteFailures       uint64
 }
 
-type dashboardData struct {
+type DashboardData struct {
 	SessionStart time.Time
 	Port         int
-	Hosts        map[string]host
+	Hosts        map[string]Host
+}
+
+type CheckLog struct {
+	Timestamp time.Time
+	Latency   uint16
+}
+
+type CheckLogMessage struct {
+	Hostname string
+	Log      CheckLog
 }
 
 func main() {
@@ -57,19 +67,22 @@ func main() {
 
 	tmpl := template.Must(template.New("dashboard.tmpl").ParseFiles("dashboard.tmpl"))
 
-	data := dashboardData{
+	data := DashboardData{
 		SessionStart: time.Now(),
 		Port:         *portPtr,
-		Hosts:        make(map[string]host),
+		Hosts:        make(map[string]Host),
 	}
+	logs := make(map[string][]CheckLog)
+	var channel chan CheckLogMessage = make(chan CheckLogMessage)
 	for hostname := range strings.SplitSeq(*hostListPtr, " ") {
-		data.Hosts[hostname] = host{
+		data.Hosts[hostname] = Host{
 			Hostname:                 hostname,
 			SessionAverageLatency:    0,
 			FiveMinuteAverageLatency: 0,
 			FiveMinuteFailures:       0,
 		}
-		go checkHost(hostname, *portPtr, interval)
+		logs[hostname] = make([]CheckLog, 0)
+		go checkHost(hostname, *portPtr, interval, channel)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -82,19 +95,70 @@ func main() {
 
 	fmt.Println("Server is running at localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	for message := range channel {
+		updateLog(message, &logs, &data.Hosts)
+	}
 }
 
-func checkHost(hostname string, port int, interval time.Duration) {
+func checkHost(hostname string, port int, interval time.Duration, channel chan<- CheckLogMessage) {
+	portStr := strconv.Itoa(port)
 	for {
-		portStr := strconv.Itoa(port)
+		startTime := time.Now()
 		conn, err := net.DialTimeout("tcp", net.JoinHostPort(hostname, portStr), time.Second)
 		if err != nil {
 			fmt.Println("Connection error:", hostname, err)
+			channel <- CheckLogMessage{Hostname: hostname, Log: CheckLog{Timestamp: time.Now(), Latency: 0}}
 		}
 		if conn != nil {
+			endTime := time.Now()
+			var latency time.Duration = endTime.Sub(startTime)
 			fmt.Println("Opened", net.JoinHostPort(hostname, portStr))
+			channel <- CheckLogMessage{Hostname: hostname, Log: CheckLog{Timestamp: time.Now(), Latency: uint16(latency.Milliseconds())}}
 			conn.Close()
+		} else {
+			channel <- CheckLogMessage{Hostname: hostname, Log: CheckLog{Timestamp: time.Now(), Latency: 0}}
 		}
 		time.Sleep(interval)
 	}
+}
+
+func updateLog(message CheckLogMessage, logs *map[string][]CheckLog, data *map[string]Host) {
+	(*logs)[message.Hostname] = append((*logs)[message.Hostname], message.Log)
+	host := (*data)[message.Hostname]
+	if message.Log.Latency > 0 {
+		host.LastSeen = message.Log.Timestamp
+	}
+
+	var fiveMinuteCalls uint = 0
+	var sessionLatency []uint16 = make([]uint16, 0)
+	var fiveMinuteLatency []uint16 = make([]uint16, 0)
+	cutoff := time.Now().Add(-5 * time.Minute)
+	for _, l := range (*logs)[message.Hostname] {
+		fiveMinutes := l.Timestamp.After(cutoff)
+		if fiveMinutes {
+			fiveMinuteCalls++
+			if l.Latency > 0 {
+				fiveMinuteLatency = append(fiveMinuteLatency, l.Latency)
+			}
+		} else if l.Latency > 0 {
+			sessionLatency = append(sessionLatency, l.Latency)
+		}
+	}
+	host.FiveMinuteAverageLatency = avgLatency(fiveMinuteLatency)
+	host.SessionAverageLatency = avgLatency(sessionLatency)
+	host.FiveMinuteFailures = uint64(fiveMinuteCalls - uint(len(fiveMinuteLatency)))
+
+	(*data)[message.Hostname] = host
+}
+
+func avgLatency(latencies []uint16) uint16 {
+	if len(latencies) == 0 {
+		return 0
+	}
+	var total uint64
+	for _, l := range latencies {
+		total = total + uint64(l)
+	}
+	return uint16(total / uint64(len(latencies)))
 }
