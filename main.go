f8d95c951b8a9ac4babf479076855e3e61727e6f
Jason Dyck <j.dyck@redskytech.io>
2025-10-22 19:44:44 -0600

    Added static dashboard

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
@@ -11,10 +12,33 @@ import (
 	"time"
 )
 
+type HostStatus string
+
+const (
+	HostStatusOnline   HostStatus = "Online"
+	HostStatusUnstable HostStatus = "Unstable"
+	HostStatusOffline  HostStatus = "Offline"
+)
+
+type host struct {
+	Hostname                 string
+	Status                   HostStatus
+	LastSeen                 time.Time
+	SessionAverageLatency    uint16
+	FiveMinuteAverageLatency uint16
+	FiveMinuteFailures       uint64
+}
+
+type dashboardData struct {
+	SessionStart time.Time
+	Port         int
+	Hosts        map[string]host
+}
+
 func main() {
 	hostListPtr := flag.String("hosts", "", "A space-delimited list of hosts to monitor.")
 	portPtr := flag.Int("port", 80, "The port to monitor on each host. Defaults to 80.")
-	intervalPtr := flag.Int("interval", 5000, "The interval on which to check each host in milliseconds. Defaults to 5000.")
+	intervalPtr := flag.Int("interval", 5000, "The interval on which to check each host in milliseconds. Minimum 1000. Defaults to 5000.")
 	flag.Parse()
 
 	trimmedList := strings.TrimSpace(*hostListPtr)
@@ -24,19 +48,36 @@ func main() {
 	if *portPtr <= 0 || *portPtr > 65535 {
 		panic("Invalid port provided")
 	}
-	if *intervalPtr <= 0 {
+	if *intervalPtr <= 1000 {
 		panic("Invalid interval provided")
 	}
 	interval := time.Duration(*intervalPtr * 1000 * 1000)
 
 	fmt.Println("Preparing to monitor port", *portPtr, "every", *intervalPtr, "milliseconds on hosts", *hostListPtr)
 
-	for host := range strings.SplitSeq(*hostListPtr, " ") {
-		go checkHost(host, *portPtr, interval)
+	tmpl := template.Must(template.New("dashboard.tmpl").ParseFiles("dashboard.tmpl"))
+
+	data := dashboardData{
+		SessionStart: time.Now(),
+		Port:         *portPtr,
+		Hosts:        make(map[string]host),
+	}
+	for hostname := range strings.SplitSeq(*hostListPtr, " ") {
+		data.Hosts[hostname] = host{
+			Hostname:                 hostname,
+			SessionAverageLatency:    0,
+			FiveMinuteAverageLatency: 0,
+			FiveMinuteFailures:       0,
+		}
+		go checkHost(hostname, *portPtr, interval)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world!")
+		err := tmpl.Execute(w, data)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(500)
+		}
 	})
 
 	fmt.Println("Server is running at localhost:8080")
